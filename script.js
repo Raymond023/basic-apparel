26a57bf6139b07afff680ba2bca8d6b56c517d2e
Raymond023 <raymondraphael11@gmail.com>
2025-02-06 08:58:56 +0100

    added email validation

--- script.js
+++ script.js
@@ -2,10 +2,20 @@
 
 const myForm = document.querySelector('form')
 const emailInput = document.querySelector('#email');
+const submit = document.querySelector('.submit');
 
 const regex = /\S+@\S+\.\S+/;
 
-if(emailInput.value !== regex) {
-    emailInput.classList.add('error')
-} 
-console.log(myForm)
+function emailValidation () {
+    if(regex.test(emailInput.value)) {
+        emailInput.classList.remove('error');
+        submit.classList.remove('show');
+    } else {
+        emailInput.classList.add('error');
+        submit.classList.add('show');
+    }
+}
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    emailValidation()
+});
